9972351a7899184082a670c747fae5f04ebbffd4
AD\u1330244 <debdipbanerjee7@gmail.com>
2022-10-09 21:21:43 -0600

    Player test state added & state switching added

--- Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStateMachine : MonoBehaviour
+public class PlayerStateMachine : StateMachine
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        SwitchState(new PlayerTestState(this));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
