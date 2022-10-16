fdc32824f801e27ed6011e1ddc70bc0567604a2d
AD\u1330244 <debdipbanerjee7@gmail.com>
2022-10-16 02:44:44 -0600

    Cinemachine target group added

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -1,9 +1,12 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Targeter : MonoBehaviour
 {
+    [SerializeField] private CinemachineTargetGroup cineTargetGroup;
+
     private List<Target> targets = new List<Target>();
 
     public Target CurrentTarget { get; private set; }
@@ -17,6 +20,7 @@ public class Targeter : MonoBehaviour
 
         // if it's a target, add it to target list
         targets.Add(target);
+        target.OnDestroyed += RemoveTarget;
     }
 
     private void OnTriggerExit(Collider other)
@@ -27,7 +31,7 @@ public class Targeter : MonoBehaviour
         }
 
         // if it's a target, remove it from target list
-        targets.Remove(target);
+        RemoveTarget(target);
     }
 
     public bool SelectTarget()
@@ -39,12 +43,31 @@ public class Targeter : MonoBehaviour
         }
 
         CurrentTarget = targets[0];
+        cineTargetGroup.AddMember(CurrentTarget.transform, 1f, 2f);
 
         return true;
     }
 
     public void Cancel()
     {
+        if (CurrentTarget == null)
+        {
+            return;
+        }
+
+        cineTargetGroup.RemoveMember(CurrentTarget.transform);
         CurrentTarget = null;
+    }
+
+    private void RemoveTarget(Target target)
+    {
+        if (CurrentTarget == target)
+        {
+            cineTargetGroup.RemoveMember(CurrentTarget.transform);
+            CurrentTarget = null;
+        }
+
+        target.OnDestroyed -= RemoveTarget;
+        targets.Remove(target);
     }
 }
